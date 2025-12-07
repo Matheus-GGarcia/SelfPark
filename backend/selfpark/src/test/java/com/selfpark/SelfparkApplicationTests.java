2d68a6a501dac9a106e0639d6b9fb1c923ab1edc
Anthony oliveira <antholi0203@gmail.com>
2025-12-07 01:05:45 +0000

    começando o back

---
+++ backend/selfpark/src/test/java/com/selfpark/SelfparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
